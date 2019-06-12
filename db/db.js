dd236f9365b39990dca04cd28826e3a33a60fbab
roshan-soni <roshan.soni@appirio.com>
2019-06-12 14:52:00 +0530

    init commit

---
+++ db/db.js
@@ -0,0 +1,116 @@
+const coffee = [
+  {
+    id: 1,
+    title: "Cafetière",
+    description:
+      "A French press, also known as a press pot, coffee press, coffee plunger, cafetière (UK) or cafetière à piston, is a basic coffee, simple to made.",
+    price: "300"
+  },
+  {
+    id: 2,
+    title: "Frappuccino",
+    description:
+      "The frappuccino is a coffee blended with ice and other ingredients and topped with whip cream.",
+    price: "300"
+  },
+  {
+    id: 3,
+    title: "Caramel Macchiato",
+    description:
+      "This refreshing and tasty iced coffee drink is yet another our classic, and just like the frappuccino, the caramel macchiato.",
+    price: "300"
+  },
+  {
+    id: 4,
+    title: "Café Mocha",
+    description:
+      "This variant of the café latte is an American invention, one inspired by the coffee beverage Bicerin. The café mocha is an espresso-based drink with hot milk and chocolate.",
+    price: "300"
+  },
+  {
+    id: 5,
+    title: "Caffé Americano",
+    description:
+      "Many espresso-based drinks use milk, but not the café Americano – or simply 'Americano'. The drink also uses espresso but is infused with hot water instead of milk. The result is a coffee beverage that has the same strength of drip coffee but with a different flavor.",
+    price: "300"
+  },
+  {
+    id: 6,
+    title: "Turkish Coffee",
+    description:
+      "Turkish coffee boasts an unrivaled taste, body, and aroma – prepared and presented in time-honored Turkish tradition.",
+    price: "300"
+  },
+  {
+    id: 7,
+    title: "Cafe Cubano",
+    description:
+      "The café cubano – also known as Cuban coffee or Cuban espresso – is a type of espresso that originated from Cuba. This espresso shot is sweetened after brewing and is used as a base for other drinks. Its strength is twice that of traditional American coffee and Cubans drink it as part of their daily morning ritual.",
+    price: "300"
+  },
+  {
+    id: 8,
+    title: "Café Latte",
+    description:
+      "Cafe latte is made from espresso (one-third), steam milk (two-thirds), topped with about a centimeter of foam. It’s one of the most popular coffee beverages around.",
+    price: "300"
+  },
+  {
+    id: 9,
+    title: "Irish Coffee",
+    description:
+      "A cocktail made of hot coffee, sugar, thick cream, and of course, Irish whiskey.",
+    price: "300"
+  },
+  {
+    id: 10,
+    title: "Espresso",
+    description:
+      "This coffee beverage is made by forcing nearly boiling water under pressure through ground coffee beans, resulting in a brew that’s thicker than those made from other brewing methods.",
+    price: "300"
+  },
+  {
+    id: 11,
+    title: "Cortado",
+    description:
+      "The cortado is a favorite in Spain, Portugal, and Latin America, often enjoyed as an afternoon drink. The beverage uses espresso that is cut by a small amount of warm milk, reducing its acidity.The milk used for the cortado is a little cooler than that of the macchiato while having the consistency of the milk used in latte. If you cannot decide between iced and hot coffee, pick the cortado for a change.",
+    price: "300"
+  },
+  {
+    id: 12,
+    title: "Affogato",
+    description:
+      "It’s hard to decide whether the affogato is a beverage or a dessert, but that hardly matters. You must try this concoction if you haven’t yet. This coffee-based beverage uses the hot and bitter espresso to melt the gelato, resulting to a creamy pool of coffee and ice cream that you’d want to drown in!",
+    price: "300"
+  },
+  {
+    id: 13,
+    title: "Cappuccino",
+    description:
+      "This Italian coffee drink is traditionally made with espresso, hot milk, and topped with foamed milk, often prepared with a milk steamer for a thicker froth.",
+    price: "300"
+  },
+  {
+    id: 14,
+    title: "Plain Coffee",
+    description:
+      "Plain coffee is special. It’s one of the most traded commodities in the world, second only to oil!. Simple combination of water and coffee.",
+    price: "300"
+  },
+  {
+    id: 15,
+    title: "Iced Coffee",
+    description:
+      "Iced coffee can be a great, refreshing beverage – but only if you can keep the drink cold without diluting it with ice.",
+    price: "300"
+  },
+  {
+    id: 16,
+    title: "Café au lait",
+    description:
+      "Café au lait – half brewed coffee, half warm milk, 100% French. The beverage’s name literally translates to “coffee with milk,” very apt as one of its essential components is the thinning of the strong coffee with warm creamy milk.",
+    price: "300"
+  }
+];
+
+export default coffee;
