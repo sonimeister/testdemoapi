f403046b130963b8f2d4bd4f0049ff21b7499979
sonimeister <soniroshan@OUTLOOK.COM>
2024-07-11 18:40:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/api/v1/coffee", (req, res) => {
     coffee: coffee,
   });
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
